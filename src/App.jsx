1edcda0a9e0aac58eab45abe4a3c5ef12881863a
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-03-10 13:15:00 +0300

    created hero section files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import "./index.css"
+import Hero from './Components/Hero/Hero'
 
 const App = () => {
   return (
     <div>
       <Navbar fixed="top"/>
+      <Hero/>
     </div>
   )
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Hero.css"
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+const Hero = () => {
+  return (
+    <div className='container'>
+
+    </div>
+  )
+}
+
+export default Hero
