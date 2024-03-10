5d6776ea4feea191b4b2ef1ad30487de9855d479
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-03-10 12:59:44 +0300

    added navbar codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import "./index.css"
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar fixed="top"/>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import "./Navbar.css"
+import React from "react";
+import "./Navbar.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import "../../index.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="background">
+      <nav className="container">
+        <img
+          className="logo"
+          src="https://greatstack.in/assets/logo-DUdXW4nF.png"
+          alt="logo"
+        />
+        <ul>
+          <li>Home</li>
+          <li>Program</li>
+          <li>About Us</li>
+          <li>Campus</li>
+          <li>Testimonials</li>
+        </ul>
+        <Button className="btn ms-0" variant="light">
+          Contact Us
+        </Button>
+      </nav>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
