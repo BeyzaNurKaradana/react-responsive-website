80141359c96b98daa186d61f00199d2588b4c592
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-03-10 17:19:18 +0300

    added about component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./index.css"
 import Hero from './Components/Hero/Hero'
 import Program from './Components/Program/Program'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero/>
       <Title subTitle={"OUR PROGRAM"} title={"What We Offer"}/>
       <Program/>
+      <About/>
     </div>
   )
 }
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./About.css";
+import { Col, Container, Row } from "react-bootstrap";
+import Title from "../Title/Title";
+
+const About = () => {
+  return (
+    <Container>
+      <Row className="program-row mb-5">
+        <Col>
+          <img
+            width={500}
+            className="aboutPhoto"
+            src="https://greatstack.in/assets/program-1-DA_EvTPy.png"
+            alt="student1"
+          />
+        </Col>
+        <Col>
+          <Title
+            subTitle={"ABOUT UNIVERSITY"}
+            title={"Nurturing Tomorrow's Leaders Today"}
+          />
+          <p>
+            Embark on a transformative educational journey with our university's
+            comprehensive education programs. Our cutting-edge curriculum is
+            designed to empower students with the knowledge, skills, and
+            experiences needed to excel in the dynamic field of education. With
+            a focus on innovation, hands-on learning, and personalized
+            mentorship, our programs prepare aspiring educators to make a
+            meaningful impact in classrooms, schools, and communities. Whether
+            you aspire to become a teacher, administrator, counselor, or
+            educational leader, our diverse range of programs offers the perfect
+            pathway to achieve your goals and unlock your full potential in
+            shaping the future of education.
+          </p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default About;
